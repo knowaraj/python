3607c58058bd06d6e71aed88f2b264d898aaf138
Araj Dulal <sendmailtoaraj@gmail.com>
2023-09-20 21:40:25 +0545

    Whole code

---
+++ Weekday.py
@@ -0,0 +1,21 @@
+#Weekdays Using match case
+x=input("Enter Your Day Number : ")
+match x:
+    case 1: 
+        print("Sunday")
+    case 2:
+        print("Monday")
+    case 3:
+        print("Tuesday")
+    case 4:
+        print("Wednesday")
+    case 5:
+        print("Thursday")
+    case 6:
+        print("Friday")
+    case 7:
+        print("Saturday")
+    case _:
+        print("Enter corect choice 1-7.")
+    
+    
